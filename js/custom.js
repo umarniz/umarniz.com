30f3223165a70f912900aaf594563d58bbaefd95
Umar Nizamani <umar.nizamani@gmail.com>
2016-02-23 18:49:44 +0500

    Added portfolio JS and improved styling

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,67 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+
+var Categories = {
+    MobileApp:   {id: "mobile-apps",   name:"Mobile Apps"},
+    MobileGame:  {id: "mobile-games",  name:"Mobile Games"},
+    Unity:        {id: "unity",         name:"Unity 3D"},
+    VR:           {id: "vr",            name:"VR"},
+    Product:      {id: "product",       name:"Products"},
+    Misc:         {id: "misc",          name:"Misc"}
+};
+
+var Projects = [
+  { title:"Ome - Android",                  image:"portfolio-1.jpg", categories: [Categories.MobileApp] },
+  { title:"Ome - iOS",                      image:"portfolio-1.jpg", categories: [Categories.MobileApp] },
+  { title:"Samsung - Art of Feeling",       image:"samsung_aof.jpg", categories: [Categories.MobileApp] },
+  { title:"ABC - Future of VR",             image:"abc_vr_demo.jpg", categories: [Categories.VR] },
+  { title:"Unilever - Interactive Table",   image:"interactive_table.jpg", categories: [Categories.Unity] },
+  { title:"Unilever - Street Window",       image:"street_window.jpg", categories: [Categories.Unity] },
+  { title:"Unilever - Aisle Leader",        image:"aisle_leader.jpg", categories: [Categories.Unity] },
+  { title:"Unilever - Transparent Fridge",  image:"transparent_fridge.jpg", categories: [Categories.Unity] },
+  { title:"Unilever - Deep Freezer",        image:"freezer.jpg", categories: [Categories.Unity] },
+  { title:"Death Mile",                     image:"death_mile.jpg", categories: [Categories.MobileGame, Categories.Unity] },
+  { title:"BLAZ3D",                         image:"portfolio-1.jpg", categories: [Categories.MobileGame] },
+  { title:"Slash UX",                       image:"portfolio-1.jpg", categories: [Categories.Product] }
+];
+
+// Fill category nodes
+var catContainer = document.getElementById("category_container");
+for (var cat in Categories) {
+  var node = document.createElement('li');
+  node.innerHTML = "<a href=\"#\" data-filter=\"." + Categories[cat].id + "\">" + Categories[cat].name + "</a>";
+  catContainer.appendChild(node);
+  console.log(node);
+}
+
+
+/*
+var categoryNode = $('#all_filters');
+var clone = element.clone();
+
+var hrefNode = clone.children('a');
+hrefNode.attr("data-filter", ".mobile-apps");
+hrefNode.text("Mobile Apps");
+element.parent().append(clone);
+*/
+
+var container = document.getElementById("portfolio_container");
+
+//for (var i=Projects.length-1;i>=0;i--) {
+for (var i=0;i<Projects.length;i++) {
+  var cProj = Projects[i];
+
+  var div = document.createElement('div');
+
+  var className = "isotope-item";
+  for (var cat in cProj.categories) {
+    className += " " + cProj.categories[cat].id;
+  }
+
+  div.setAttribute("class", className);
+
+  div.innerHTML = "<div class=\"image-box\"><div class=\"overlay-container\"><img src=\"images/" + cProj.image + "\" alt=\"\"><a class=\"overlay\" data-toggle=\"modal\" data-target=\"#project-" + i +"\"><div class=\"bottom-text\"><span>" + cProj.title + "</span></div></a></div></div>"
+  container.appendChild(div);
+  console.log(className);
+}
