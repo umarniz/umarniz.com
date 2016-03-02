0f1cf9d8aaac30973bdfbacfd72e7a4da3557961
Umar Nizamani <umar@nerdiacs.com>
2016-03-02 22:30:49 +0500

    Added more content. Added new gallery Centered gallery content Added pause Vimeo/Youtube on modal hide.

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,10 @@
-/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
- * Author:HtmlCoder
- * Author URI:http://www.htmlcoder.me
- * Version:1.0.0
- * Created:November 2014
- * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
- * File Description: Place here your custom scripts
- */
+/*
+  JS for umarniz.com
+*/
 
  var $loading = $('.spinner').hide();
- console.log("Hiding " + $loading);
  $(document)
  .ajaxStart(function () {
-   console.log("Spinner!");
    $loading.show();
  })
  .ajaxStop(function () {
@@ -31,13 +24,16 @@ var Projects = [
   { title:"Ome",                            id:"ome", categories: [Categories.MobileApp] },
   { title:"Samsung - Art of Feeling",       id:"samsung_aof", categories: [Categories.MobileApp] },
   { title:"ABC - Future of VR",             id:"abc_vr_demo", categories: [Categories.VR] },
+  { title:"Accenture Grabber",              id:"accenture_grabber", categories: [Categories.MobileApp] },
+  { title:"Smart Guide",                    id:"smart_guide", categories: [Categories.MobileApp] },
   { title:"Unilever - Interactive Table",   id:"interactive_table", categories: [Categories.Unity] },
   { title:"Unilever - Street Window",       id:"street_window", categories: [Categories.Unity] },
   { title:"Unilever - Aisle Leader",        id:"aisle_leader", categories: [Categories.Unity] },
   { title:"Unilever - Transparent Fridge",  id:"transparent_fridge", categories: [Categories.Unity] },
   { title:"Unilever - Deep Freezer",        id:"freezer", categories: [Categories.Unity] },
+  { title:"Dove - Hero Bar",                id:"dove_hero_bar", categories: [Categories.Misc] },
   { title:"Interactive Music",              id:"interactive_music", categories: [Categories.Misc] },
-  { title:"Circuit Valencia",               id:"circuit_valencia", categories: [Categories.VR] },
+  { title:"Circuit Valencia",               id:"circuit_valencia", categories: [Categories.VR, Categories.Unity] },
   { title:"Death Mile",                     id:"death_mile", categories: [Categories.MobileGame, Categories.Unity] },
   { title:"BLAZ3D",                         id:"blaz3d", categories: [Categories.MobileGame] },
   { title:"Slash UX",                       id:"portfolio-1", categories: [Categories.Product] }
@@ -49,22 +45,12 @@ for (var cat in Categories) {
   var node = document.createElement('li');
   node.innerHTML = "<a data-filter=\"." + Categories[cat].id + "\">" + Categories[cat].name + "</a>";
   catContainer.appendChild(node);
-  console.log(node);
+  // console.log(node);
 }
 
-/*
-var categoryNode = $('#all_filters');
-var clone = element.clone();
-
-var hrefNode = clone.children('a');
-hrefNode.attr("data-filter", ".mobile-apps");
-hrefNode.text("Mobile Apps");
-element.parent().append(clone);
-*/
-
+// Fill portfolio objects
 var container = document.getElementById("portfolio_container");
 
-//for (var i=Projects.length-1;i>=0;i--) {
 for (var i=0;i<Projects.length;i++) {
   var cProj = Projects[i];
 
@@ -80,68 +66,103 @@ for (var i=0;i<Projects.length;i++) {
   div.setAttribute("class", className);
 
   div.innerHTML = "<div class=\"image-box\"><div class=\"overlay-container\"><img src=\"images/" + cProj.id + ".jpg" + "\" alt=\"\"><a class=\"overlay\" data-toggle=\"modal\" data-target=\"#" + cProj.id + "\"><div class=\"bottom-text\"><span>" + cProj.title + "</span></div></a></div></div>"
+
+  var modalContent = $('#' + cProj.id)
+  if  (modalContent != null) {
+    var title = modalContent.find('.modal-title');
+    if (title) {
+      title.text(cProj.title);
+    }
+  }
+
   container.appendChild(div);
-  console.log(className);
 }
 
 $('body').on("show.bs.modal", function(e) {
       var link = $(e.relatedTarget);
       var body = $(this).find(link.attr('data-target')).find(".modal-body");
 
-      /*if (body.children().length == 0 ) {
-        body.load(link.attr("href"));
-      } else {*/
-      if (body.find('video')[0] != null)
+      if (body.find('video')[0] != null) {
         body.find('video')[0].play();
-      //}
-
+      }
   });
 
 $('body').on('hidden.bs.modal', '.modal', function () {
-  console.log("Hiding modal");
+  // Pause HTML5 Videos
   $(this).find('video').each(function(num,val){
       this.pause();
       this.currentTime = 0;
   });
+
+  // Pause Vimeo videos
+  $(this).find('.vimeo').each(function(num,val) {
+    var contentWindow = $(this)[0].contentWindow;
+    var targetOriginUrl = $(this).attr('src').split('?')[0];
+    contentWindow.postMessage({ 'method': 'pause' }, targetOriginUrl);
+  });
+
+  // Pause Youtube videos
+  $(this).find('.youtube').each(function(num,val) {
+    var contentWindow = $(this)[0].contentWindow;
+    var targetOriginUrl = $(this).attr('src').split('?')[0];
+    contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+  });
+
 });
 
-jQuery(document).ready(function ($) {
-    var jssor_1_SlideshowTransitions = [
-      {$Duration:1200,$Opacity:2}
-    ];
+// Setup carousels
+$('.carousel').each(function() {
+  var carouselID = $(this).attr('id');
 
-    var jssor_1_options = {
-      $AutoPlay: true,
-      $SlideshowOptions: {
-        $Class: $JssorSlideshowRunner$,
-        $Transitions: jssor_1_SlideshowTransitions,
-        $TransitionsOrder: 1
-      },
-      $ArrowNavigatorOptions: {
-        $Class: $JssorArrowNavigator$
-      },
-      $BulletNavigatorOptions: {
-        $Class: $JssorBulletNavigator$
-      }
-    };
+  console.log("Carousel - " + carouselID );
 
-    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+  var ratio = '56.25%';
 
-    //responsive code begin
-    //you can remove responsive code if you don't want the slider scales while window resizing
-    function ScaleSlider() {
-        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-        if (refSize) {
-            refSize = Math.min(refSize, 600);
-            jssor_1_slider.$ScaleWidth(refSize);
-        }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
-    }
-    ScaleSlider();
-    $(window).bind("load", ScaleSlider);
-    $(window).bind("resize", ScaleSlider);
-    $(window).bind("orientationchange", ScaleSlider);
-    //responsive code end
+  if (carouselID == 'freezer_carousel') {
+    ratio = '56.25%';
+  } else if (carouselID == 'deathmile_carousel') {
+    ratio = '56.34%';
+  } else if (carouselID == 'smartguide_carousel') {
+    ratio = '56.91%';
+  }
+
+  console.log("Ratop - " + ratio);
+
+  $(this).find('.carousel .item').each(function() {
+    console.log("Setting Carousel");
+   $(this).height(0);
+   $(this).css('padding-top', ratio);
+   $(this).addClass('full-screen');
+ });
+
+ var $item = $(this).find('.item');
+ var $numberofSlides = $(this).find('.item').length;
+ var $currentSlide = 0;
+
+ $(this).find('.carousel-indicators li').each(function(){
+   var $slideValue = $(this).attr('data-slide-to');
+   if($currentSlide == $slideValue) {
+     $(this).addClass('active');
+     $item.eq($slideValue).addClass('active');
+   } else {
+     $(this).removeClass('active');
+     $item.eq($slideValue).removeClass('active');
+   }
+ });
+
+ $(this).find('.carousel img').each(function() {
+   var $src = $(this).attr('src');
+   var $color = $(this).attr('data-color');
+   $(this).parent().css({
+     'background-image' : 'url(' + $src + ')',
+     'background-color' : $color
+   });
+   $(this).remove();
+ });
+
+  // Initialize all caoursels
+  $(this).carousel({
+    interval: 0,
+    pause: "true"
+  });
 });
