04bb791713103217fd41be31663f6a87e2e49a69
Umar Nizamani <umar.nizamani@gmail.com>
2022-09-27 13:32:25 +0200

    Updated my current role

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ export default function BlogIndex({ data }) {
       <Helmet title={config.siteTitle} />
       <SEO />
       <Blurb title="Hey, I'm Umar!">
-        <h5> CTO, NiceDay Nederland </h5>
+        <h5> Managing Director, <a href="https://nicedaytherapy.com">NiceDay International</a></h5>
         <p>
           15 years experience building 1-50 people companies.
           Engineer and tinkerer at heart. Love building products.
