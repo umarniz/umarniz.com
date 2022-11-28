968af3f6fbba9edc95b34505e0314be4c29a50d5
Umar Nizamani <umar.nizamani@gmail.com>
2022-11-28 12:17:16 +0100

    Added EMHIC 22

--- src/data/speaking.js
+++ src/data/speaking.js
@@ -2,11 +2,16 @@ import android from '/static/icons/android.png'
 
 export default [
   {
+    id: 'emhic-congress-2022',
+    description: 'eMental Health International Congress, 2022',
+    title: 'Building AI responsibly for mental healthcare',
+    path: 'https://www.youtube.com/watch?v=T6PeepWOBmE',
+  },
+  {
     id: 'continuous-lifecycle-2019',
     description: 'Continuous Lifecycle London, 2019',
     title: 'Revamping A Legacy Backend',
     path: 'https://www.youtube.com/watch?v=k31wtJ7zq_w',
-    // staticThumbnail: android,
   },
   {
     id: 'code-europe-2017',
