f1ef69bd2caa8d7e50ea9dcd2c820a1930d97049
Umar Nizamani <umar.nizamani@gmail.com>
2024-08-04 16:57:21 +0200

    Changed logic of loading request sentinel

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,9 +21,11 @@ export default function Layout({ children }) {
         <html lang="en"/>
         <link rel="shortcut icon" type="image/png" />
         <script defer src="https://cloud.umami.is/script.js" data-website-id="496097ed-8151-4c01-9f87-ccab688479ee"></script>
-        <script src="https://unpkg.com/@request-sentinel/request-sentinel-js@0.1.0/dist/index.js"></script>
+        <script src="https://unpkg.com/@request-sentinel/request-sentinel-js@0.1.0/dist/index.js" async></script>
         <script>
+          window.onload = function() {
             RequestSentinel.init('l22CzMpSsQSdq3rqWcgPhojXGHfkA2Ne', '1.0', 'prod');
+          };
         </script>
       </Helmet>
       <Nav />
