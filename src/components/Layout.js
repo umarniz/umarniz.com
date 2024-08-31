48535221c3286320de6b4bc2b78951ec88cf13a8
Umar Nizamani <umar.nizamani@gmail.com>
2024-08-31 17:53:15 +0200

    Add request sentinel

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,21 +12,19 @@ import '../new-moon.css'
 import VideoPlayer from './mdx/VideoPlayer'
 import SlidesViewer from './mdx/SlidesViewer'
 
+import RequestSentinel from '@request-sentinel/request-sentinel-js'
+
 const mdxComponents = {VideoPlayer, SlidesViewer}
 
 export default function Layout({ children }) {
+  RequestSentinel.init('l22CzMpSsQSdq3rqWcgPhojXGHfkA2Ne', '1.0', 'prod', {debug: true})
+
   return (
     <>
       <Helmet>
         <html lang="en"/>
         <link rel="shortcut icon" type="image/png" />
         <script defer src="https://cloud.umami.is/script.js" data-website-id="496097ed-8151-4c01-9f87-ccab688479ee"></script>
-        <script src="https://unpkg.com/@request-sentinel/request-sentinel-js@0.1.0/dist/index.js" async></script>
-        <script>
-          window.onload = function() {
-            RequestSentinel.init('l22CzMpSsQSdq3rqWcgPhojXGHfkA2Ne', '1.0', 'prod')
-          }
-        </script>
       </Helmet>
       <Nav />
       <main className="main-container">
